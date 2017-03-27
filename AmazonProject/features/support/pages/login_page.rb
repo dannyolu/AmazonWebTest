74a247c563e83debf4b76e77e363a3f769aa1bb0
Daniel Olumese <dannyolumese@gmail.com>
2017-03-27 01:12:00 +0100

    Update code

--- AmazonProject/features/support/pages/login_page.rb
+++ AmazonProject/features/support/pages/login_page.rb
@@ -2,14 +2,18 @@ require_relative '../pages/base_page'
 
 class LoginPage < BasePage
 
+  USERNAME_LOCATOR = {:id => 'ap_email'}
+  PASSWORD_LOCATOR ={:id => 'ap_password'}
+  LOGIN_BUTTON_LOCATOR = {:id => 'signInSubmit'}
+
   def initialize(browser)
     super(browser)
   end
 
   def login_with(username, password)
-    @browser.find_element(:id => 'ap_email').send_keys(username)
-    @browser.find_element(:id => 'ap_password').send_keys(password)
-    @browser.find_element(:id => 'signInSubmit').click
+    @browser.find_element(USERNAME_LOCATOR).send_keys(username)
+    @browser.find_element(PASSWORD_LOCATOR).send_keys(password)
+    @browser.find_element(LOGIN_BUTTON_LOCATOR).click
     return WelcomePage.new(@browser)
   end
 
